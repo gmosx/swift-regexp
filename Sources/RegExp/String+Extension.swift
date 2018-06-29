5dde326ae646e6631bcff1fabfba1fbce4423c6b
George Moschovitis <gmosx@reizu.com>
2018-06-29 22:37:52 +0300

    Added operators

--- Sources/RegExp/String+Extension.swift
+++ Sources/RegExp/String+Extension.swift
@@ -1,5 +1,21 @@
 extension String {
+    public func isMatching(pattern: StringPattern) -> Bool {
+        return pattern.hasMatch(in: self)
+    }
+
     public func replacing(pattern: StringPattern, handler: (StringMatch) -> String?) -> String {
         return pattern.replace(in: self, handler: handler)
     }
+}
+
+infix operator =~
+
+public func =~(string: String, pattern: StringPattern) -> Bool {
+    return string.isMatching(pattern: pattern)
+}
+
+infix operator !~
+
+public func !~(string: String, pattern: StringPattern) -> Bool {
+    return !string.isMatching(pattern: pattern)
 }
--- Tests/RegExpTests/StringTests.swift
+++ Tests/RegExpTests/StringTests.swift
@@ -12,6 +12,18 @@ class StringTests: XCTestCase {
         XCTAssertEqual(replaced, "[bad] wolf, [bad] dog, [bad] sheep")
     }
 
+    func testIsMatching() {
+        let text = "We are big now #1#**lot of sales#/1#* the money and cards #2#Rober Langdon and Ambra Vidal#/2#**."
+        let re = RegExp("(#/{0,1}\\d{1,}#\\*{0,2})")
+        
+        XCTAssertTrue(text =~ re)
+        
+        let nonMatchingText = "I do not match"
+        
+        XCTAssertTrue(nonMatchingText !~ re)
+        XCTAssertFalse(nonMatchingText =~ re)
+    }
+
     static var allTests = [
         ("testReplacingPattern", testReplacingPattern),
     ]
